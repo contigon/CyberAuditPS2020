2148f413cd5b9c059680957e8214abe905cc4e2d
contigon <omer.friedman1@gmail.com>
2020-04-22 16:17:41 +0300

    auto commit from powershell script

--- CyberCompressGo.ps1
+++ CyberCompressGo.ps1
@@ -57,12 +57,12 @@ $compressUpdates = @{
   Path = "$PSScriptRoot\cyberAnalyzers.ps1",
           "$PSScriptRoot\cyberAudit.ps1",
           "$PSScriptRoot\cyberBuild.ps1",
+          "$PSScriptRoot\cyberAttack.ps1",
           "$PSScriptRoot\CyberCollectNetworkConfig.ps1",
           "$PSScriptRoot\CyberCompressGo.ps1",
           "$PSScriptRoot\CyberCreateRunecastRole.ps1",
           "$PSScriptRoot\CyberFunctions.ps1",
           "$PSScriptRoot\CyberLicenses.ps1",
-          "$PSScriptRoot\cyberAttack.ps1",
           "$PSScriptRoot\CyberPasswordStatistics.ps1",
           "$PSScriptRoot\CyberPingCastle.ps1",
           "$PSScriptRoot\CyberAuditDevelopersHelp.txt",
@@ -71,12 +71,12 @@ $compressUpdates = @{
   DestinationPath = "$PSScriptRoot\goUpdate.zip"
 }
 
-<#
-$a = ($compress['path'] -join ",").Replace("$PSScriptRoot\","")
+
+$a = ($compressUpdates['path'] -join ",").Replace("$PSScriptRoot\","")
 git add $a
-git commit -m "auto commit from omer"
+git commit -m "auto commit from powershell script"
 git push
-#>
+
 
 $c = $compress['path']
 Write-Host "Files ($c) will be compressed now" -ForegroundColor Green
