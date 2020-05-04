cd1fc53906d96018c21b1be9d11f5ab89ca4409c
contigon <omer.friedman1@gmail.com>
2020-05-04 12:03:07 +0300

    commited by Omerf from CYBER-AUDIT-PC

--- CyberCompressGo.ps1
+++ CyberCompressGo.ps1
@@ -108,7 +108,7 @@ if ((Test-Path "$PSScriptRoot\Downloads\go.pdf") -and (Test-Path "$PSScriptRoot\
     Write-Host ""
     try {
         Get-SFTPSession | ForEach {Remove-SFTPSession $_.SessionId}
-        $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "3347985_cyber","o-iyFEmj8qIN@e7E"
+        $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "3347985_cyber","blackcat2020"
         $SftpSess = New-SFTPSession -ComputerName cyberaudittool.c1.biz -Port 221 -Credential $credential -Verbose
         Set-SFTPFile -SessionId $SftpSess.SessionId -LocalFile "$PSScriptRoot\Downloads\go.ps1" -RemotePath "/cyberaudittool.c1.biz/" -Overwrite
         Set-SFTPFile -SessionId $SftpSess.SessionId -LocalFile "$PSScriptRoot\go.pdf" -RemotePath "/cyberaudittool.c1.biz/" -Overwrite
