0f927fe5c680624cba41e80ed2168db1dc3bec3c
contigon <omer.friedman1@gmail.com>
2020-08-10 15:52:48 +0300

    Update cyberAnalyzers.ps1

--- cyberAnalyzers.ps1
+++ cyberAnalyzers.ps1
@@ -145,10 +145,10 @@ switch ($input) {
         else
         {
              Write-Host "[Failure] Please install Microsoft Excel before continuing running this analysis" -ForegroundColor Red
-             read-host ìPress [Enter] if you installed Excel (or Ctrl + c to quit)î
+             read-host ‚ÄúPress [Enter] if you installed Excel (or Ctrl + c to quit)‚Äù
         }
         
-        read-host ìPress ENTER to continueî
+        read-host ‚ÄúPress ENTER to continue‚Äù
         $null = start-Process -PassThru explorer $ACQ
      }
 
@@ -188,7 +188,7 @@ switch ($input) {
         {
             Write-Host "[Failed] The file $ACQ\hashes-Ophcrack.txt was not found, please check and try again" -ForegroundColor Red
         }
-        read-host ìPress ENTER to continueî
+        read-host ‚ÄúPress ENTER to continue‚Äù
      }
        #hashcat
      3 {
@@ -303,7 +303,7 @@ switch ($input) {
             }
         }
      Pop-Location
-     read-host ìPress ENTER to continueî
+     read-host ‚ÄúPress ENTER to continue‚Äù
      }
      #BloodHound
      4 {
@@ -318,12 +318,12 @@ switch ($input) {
         within an Active Directory environment and dentify highly complex attack paths.
 
         Data Includes:
-        - Users         ñ The users on the network extracted from active directory
-        - Computers     ñ The different endpoints on the network, servers, workstations and other devices
-        - Groups        ñ The different AD groups extracted from AD
-        - Sessions      ñ The amount of user sessions on computers on the network that the ingestor has extracted
-        - ACLs          ñ Access control lists, the different permissions and access that users and groups have against each other
-        - Relationships ñ The different relations that all of the other aspects have to each other such as 
+        - Users         ‚Äì The users on the network extracted from active directory
+        - Computers     ‚Äì The different endpoints on the network, servers, workstations and other devices
+        - Groups        ‚Äì The different AD groups extracted from AD
+        - Sessions      ‚Äì The amount of user sessions on computers on the network that the ingestor has extracted
+        - ACLs          ‚Äì Access control lists, the different permissions and access that users and groups have against each other
+        - Relationships ‚Äì The different relations that all of the other aspects have to each other such as 
                           group memberships, users, user sessions and other related information
 
         Please wait untill the neo4j database is running, this can take some time,
@@ -364,9 +364,9 @@ switch ($input) {
         else {
                 $cmd = "neo4j install-service"
                 Invoke-Expression $cmd
-                Copy-Item "$appsDir\BloodHoundExampleDB\current\BloodHoundExampleDB.db\" "$appsDir\neo4j\current\data\databases\BloodHoundExampleDB.db" -Recurse -Force      
-                (Get-Content -Path  $appsDir\neo4j\current\conf\neo4j.conf -Raw) -replace "#dbms.active_database=graph.db","dbms.active_database=BloodHoundExampleDB.graphdb" | set-content -Path $appsDir\neo4j\current\conf\neo4j.conf
-                (Get-Content -Path  $appsDir\neo4j\current\conf\neo4j.conf -Raw) -replace "#dbms.allow_upgrade=true","dbms.allow_upgrade=true" | set-content -Path $appsDir\neo4j\current\conf\neo4j.conf
+                #Copy-Item "$appsDir\BloodHoundExampleDB\current\BloodHoundExampleDB.db\" "$appsDir\neo4j\current\data\databases\BloodHoundExampleDB.db" -Recurse -Force      
+                #(Get-Content -Path  $appsDir\neo4j\current\conf\neo4j.conf -Raw) -replace "#dbms.active_database=graph.db","dbms.active_database=BloodHoundExampleDB.graphdb" | set-content -Path $appsDir\neo4j\current\conf\neo4j.conf
+                #(Get-Content -Path  $appsDir\neo4j\current\conf\neo4j.conf -Raw) -replace "#dbms.allow_upgrade=true","dbms.allow_upgrade=true" | set-content -Path $appsDir\neo4j\current\conf\neo4j.conf
                 $j = Start-Job -ScriptBlock {Start-service "neo4j" -Verbose}
                 $j | Wait-Job
 	            write-host "If you have problem with service starting, please start it manually from services.msc"
@@ -383,32 +383,39 @@ switch ($input) {
 	            write-host "User:     neo4j" 
 	            write-host "Password: neo4j"
 	            write-host "Please change default password to: BloodHound" -ForegroundColor Red
-                read-host  ìAfter password is changed, Press ENTER to continueî
+                read-host  ‚ÄúAfter password is changed, Press ENTER to continue‚Äù
                 write-host "Login to the BloodHound application using:"
                 write-host "User:     neo4j" -ForegroundColor Yello
                 write-host "Password: BloodHound" -ForegroundColor Yello
                 invoke-expression "BloodHound"
         }
         Start-Process iexplore $ACQ
-        read-host ìPress ENTER to continueî
+        read-host ‚ÄúPress ENTER to continue‚Äù
      }
-     #Ophcrack
+     #PolicyAnalizer
      5 {
         CLS
         $ACQ = ACQA("GPO")
+        $SamplePolicyRulesPath =  scoop prefix PolicyAnalyzer
+        $baselinePath =  scoop prefix PolicyAnalyzerSecurityBaseline
+        Copy-Item -Path $baselinePath\* -Destination $SamplePolicyRulesPath\SamplePolicyRules -Exclude *.json -Recurse
         $help = @"
         
-        In order to add the backup GPO please follow this help:
+        In order to add the GPO backup please follow this help:
         1. Press ADD
         2. Choose File --> Add Files From GPO
         3. Path for GPO files is:  $ACQ
         4. Save this GPO (you will 1st need to give a name for this GPO)
 
+        In order to compare to baselines:
+        5. Set the Policy Rule Set path to: $SamplePolicyRulesPath
+        6. Import matching policies such as: $SamplePolicyRulesPath\Windows 10 Version 2004 and Windows Server Version 2004 Security Baseline
+
 "@
         write-host $help
         $cmd = "policyanalyzer"
         Invoke-Expression $cmd
-        read-host ìPress ENTER to continueî     
+        read-host ‚ÄúPress ENTER to continue‚Äù     
      }
      #Statistics
      6 {     
@@ -474,9 +481,9 @@ switch ($input) {
         else
         {
              Write-Host "[Failure] Please install Microsoft Excel before continuing running this analysis" -ForegroundColor Red
-             read-host ìPress [Enter] if you installed Excel (or Ctrl + c to quit)î
+             read-host ‚ÄúPress [Enter] if you installed Excel (or Ctrl + c to quit)‚Äù
         }
-        read-host ìPress ENTER to continueî
+        read-host ‚ÄúPress ENTER to continue‚Äù
      }
 
     #DSInternals
@@ -508,7 +515,7 @@ switch ($input) {
             $pwndfile = Get-FileName
             Get-ADDBAccount -All -DatabasePath $ACQ\ntds.dit -BootKey $bk | Test-PasswordQuality -WeakPasswordHashesSortedFile $pwndfile
          }
-        read-host ìPress ENTER to continueî     
+        read-host ‚ÄúPress ENTER to continue‚Äù     
      }
      #appInspector
      8 {
@@ -545,7 +552,7 @@ switch ($input) {
         Invoke-Expression $cmd
         Start-Process iexplore $ACQ
         Pop-Location    
-        read-host ìPress ENTER to continueî     
+        read-host ‚ÄúPress ENTER to continue‚Äù     
      }
      #Remediation Efforts and Risk Charts
      9 {
@@ -587,7 +594,7 @@ switch ($input) {
         {
              failed " Please install Microsoft Excel before running this analysis"
         }
-        read-host ìPress ENTER to continueî
+        read-host ‚ÄúPress ENTER to continue‚Äù
      }
      #Offline NTDS
      10 {
@@ -644,7 +651,7 @@ switch ($input) {
         write-host $help
         $ScriptToRun = $PSScriptRoot+"\CyberOfflineNTDS.ps1"
         &$ScriptToRun
-        read-host ìPress ENTER to continueî     
+        read-host ‚ÄúPress ENTER to continue‚Äù     
      }
 
 #End Menu
